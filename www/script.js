ae5b186c568109bb96950ec4a512027ebcb6a017
shimizuyusuke <yusuke_0801@softbank.ne.jp>
2017-12-14 17:32:49 +0900

    commit

---
+++ www/script.js
@@ -0,0 +1,11 @@
+function goukei(otona,kodomo){
+    return otona*500+kodomo*200+"円です。";
+}
+//Aグループ
+document.write(goukei(2,4)+"<br>");
+
+//Bグループ
+document.write(goukei(1,5)+"<br>");
+
+//Cグループ
+document.write(goukei(3,7)+"<br>");
