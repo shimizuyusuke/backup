dd39527f4481a3474f350bb9f1b276b4f05d46af
GitHubshimizuyusuke <GitHubyusuke_0801@softbank.ne.jp>
2017-12-25 17:32:43 +0900

    12/25

---
+++ moji/src/moji/moji.java
@@ -0,0 +1,31 @@
+/**
+ *
+ */
+package moji;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author internousdev
+ *
+ */
+public class moji {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		// TODO 自動生成されたメソッド・スタブ
+
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("入力してください");
+		String t=br.readLine();
+		System.out.println(t);
+
+
+	}
+
+}
