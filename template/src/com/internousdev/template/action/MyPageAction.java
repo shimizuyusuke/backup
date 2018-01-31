ad708beea8427cb3fa1c604c13d8e5a2755eac82
shimizuyusuke <yusuke_0801@softbank.ne.jp>
2018-01-31 13:53:31 +0900

    1/31

--- template/src/com/internousdev/template/action/MyPageAction.java
+++ template/src/com/internousdev/template/action/MyPageAction.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import com.internousdev.template.dao.MyPageDAO;
+import com.internousdev.template.dto.MyPageDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 
